d0ccd444f0b00130dac972cdaea911d47a03cf2d
Nicholas Young <nyoung@uptime.ventures>
2016-12-18 12:51:22 -0700

    Tweak it

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env /bin/sh
 
 curl -sfLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
-nvim -c "PlugInstall | qall | silent!" ~/.config/nvim/init.vim
+nvim -c "silent | PlugInstall | qall" ~/.config/nvim/init.vim
