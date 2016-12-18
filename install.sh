6488a0821073968ea8709a4b7b104281267c6cb5
Nicholas Young <nyoung@uptime.ventures>
2016-12-17 18:15:34 -0700

    Remove clone from install script

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env /bin/sh
 
-git clone git@github.com:nicholaswyoung/dotfiles.git ~/.config && \
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
 nvim -c "PlugInstall | qall!" ~/.config/nvim/init.vim
